a35eb5843e6e8f861282451a826c5612ff907276
Shraddha Ravishankar <shraddha.ravishanker@version1.com>
2021-11-29 09:01:31 +0000

    commented variables

--- 04-provider.tf
+++ 04-provider.tf
@@ -4,9 +4,9 @@
 
 provider "oci" {
   region           = var.region
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
+ # tenancy_ocid     = var.tenancy_ocid
+ # user_ocid        = var.user_ocid
+ # fingerprint      = var.fingerprint
   private_key_path = var.private_key_path
 }
 
@@ -16,9 +16,9 @@ provider "oci" {
 provider "oci" {
   alias            = "home"
   region           = data.oci_identity_regions.home-region.regions[0]["name"]
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
+  #tenancy_ocid     = var.tenancy_ocid
+  #user_ocid        = var.user_ocid
+  #fingerprint      = var.fingerprint
   private_key_path = var.private_key_path
 }
 
