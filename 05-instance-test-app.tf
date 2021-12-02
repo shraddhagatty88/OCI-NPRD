f3db951df30aa4125edb12984263e2b9785f6e54
Shraddha Ravishankar <shraddha.ravishanker@version1.com>
2021-12-02 07:48:38 +0000

    few changes

--- 05-instance-test-app.tf
+++ 05-instance-test-app.tf
@@ -14,8 +14,7 @@ module "instance_test_app" {
   fault_domain        = 1
   compartment_id      = var.compartment_ocid
   subnet_id           = var.subnet_ocid
-  network_sec_groups  = [ var.nsg1, var.nsg2 ]
-                    ]
+  #network_sec_groups  = [ var.nsg1, var.nsg2 ]
   ssh_authorized_keys = var.ssh_key_nonprod
   source_id           = data.oci_core_images.custom_image_v1-oel7-golden.images[0].id
   boot_volume_size_in_gbs = 100
